6cc3b121d5b47fc4663f7d3ce0bb321b689584c3
Chad Jablonski <chad@dinocore.net>
2017-11-27 19:07:53 -0800

    Add Mox as dependency in test env

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Pubsub.Mixfile do
       app: :pubsub,
       version: "0.1.0",
       elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -19,6 +20,9 @@ defmodule Pubsub.Mixfile do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -27,6 +31,7 @@ defmodule Pubsub.Mixfile do
       {:grpc, path: "../grpc-elixir"},
       {:goth, "~> 0.7"},
       {:apex, "~> 1.2", only: :dev},
+      {:mox, "~> 0.3", only: :test},
     ]
   end
 end
