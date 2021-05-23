3fa37ae30c43702253689cc5674f24105c378ab4
Chad Jablonski <chad@jablonski.xyz>
2021-05-22 22:56:40 -0400

    Create Nix flake for development

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "An Elixir client for Google Pub/Sub";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+  let
+    name = "weddell";
+  in
+  utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlay = [];
+        };
+        buildInputs = with pkgs; [
+          elixir
+          rebar3
+        ];
+        nativeBuildInputs = with pkgs; [];
+        buildEnvVars = {};
+      in rec {
+        #packages.${name} =
+        ## `nix build`
+        #defaultPackage = packages.${name};
+        ## `nix run`
+        #apps.${name} = utils.lib.mkApp {
+        #  inherit name;
+        #  drv = packages.${name};
+        #};
+        #defaultApp = apps.${name};
+        # `nix develop`
+        devShell = pkgs.mkShell
+          {
+            inherit buildInputs nativeBuildInputs;
+          } // buildEnvVars;
+      }
+    );
+}
